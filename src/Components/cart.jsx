3e267d58fcb177d55f66701f9ce7d5ce824052fc
sarand3011 <sarand3011@users.noreply.github.com>
2021-07-06 10:23:55 +0530

    First commit

---
+++ src/Components/cart.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Cart extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hellow Word</h1>
+        <button>increament</button>
+      </div>
+    );
+  }
+}
+
+export default Cart;
