30c0e54a09ae124b8836bcdedd44f96c1984fca0
sarand3011 <sarand3011@users.noreply.github.com>
2021-07-06 11:57:19 +0530

    applying styles differently

--- src/Components/cart.jsx
+++ src/Components/cart.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 
 class Cart extends Component {
+  state = {
+    count: 0,
+    // imgUrl: "https://picsum.photos/300",
+  };
+  Styles = {
+    fontSize: 25,
+    fontWeight: "bold",
+  };
+
   render() {
     return (
       <div>
-        <h1>Hellow Word</h1>
-        <button>increament</button>
+        <span style={{ fontSize: 25 }} className="badge badge-primary p-2 m-2">
+          {this.counter()}
+        </span>
+        <button style={this.Styles} className="btn btn-secondary btn-sm">
+          +
+        </button>
       </div>
     );
+  }
+  counter() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
