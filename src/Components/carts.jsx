4e2155332b885ecbf9fa147fb24154dc2f346f08
sarand3011 <sarand3011@users.noreply.github.com>
2021-07-09 17:56:21 +0530

    Delete button added

--- src/Components/carts.jsx
+++ src/Components/carts.jsx
@@ -3,17 +3,24 @@ import Cart from "./cart";
 class Carts extends Component {
   state = {
     items: [
-      { id: 1, value: 4 },
+      { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
     ],
   };
+  handleDelete = (cartId) => {
+    // this.setState({ value: 0 });
+    console.log(cartId);
+    this.setState({
+      items: this.state.items.filter((item) => item.id !== cartId),
+    });
+  };
   render() {
     return (
       <div>
         {this.state.items.map((item) => (
-          <Cart key={item.id} value={item.value} selected={true} />
+          <Cart key={item.id} onDelete={this.handleDelete} item={item} />
         ))}
       </div>
     );
